48be06b2b9e7ac902945b31d12e17f5c0845918a
Benjamin <1234hteimaj4321@gmail.com>
2020-11-02 15:41:44 -0700

    added a shuffling effect for all components that need it

--- src/containers/BasicsContainer.js
+++ src/containers/BasicsContainer.js
@@ -8,68 +8,78 @@ import { FillInTheBlank } from "../components/BasicsContainer/FillInTheBlank.com
 
 const BasicsContainer = (props) => {
   // Intro Section
+  const words = [
+    { word: "Chien", id: 1, isCorrect: false, isCurrent: false },
+    { word: "Chat", id: 2, isCorrect: false, isCurrent: false },
+    { word: "Personne", id: 3, isCorrect: false, isCurrent: false },
+    { word: "Oiseau", id: 4, isCorrect: false, isCurrent: false },
+  ];
+  const imgs = [
+    {
+      url: "https://cdn.images.express.co.uk/img/dynamic/1/590x/dog-650299.jpg",
+      id: 1,
+      isCorrect: false,
+      isCurrent: false,
+    },
+    {
+      url:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
+      id: 2,
+      isCorrect: false,
+      isCurrent: false,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      id: 3,
+      isCorrect: false,
+      isCurrent: false,
+    },
+    {
+      url:
+        "https://images.pexels.com/photos/2662434/pexels-photo-2662434.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      id: 4,
+      isCorrect: false,
+      isCurrent: false,
+    },
+  ];
+
   const [introContent, updateIntroContent] = useState({
-    words: [
-      { word: "Chien", id: 1, isCorrect: false, isCurrent: false },
-      { word: "Chat", id: 2, isCorrect: false, isCurrent: false },
-      { word: "Personne", id: 3, isCorrect: false, isCurrent: false },
-      { word: "Oiseau", id: 4, isCorrect: false, isCurrent: false },
-    ],
-    imgs: [
-      {
-        url:
-          "https://cdn.images.express.co.uk/img/dynamic/1/590x/dog-650299.jpg",
-        id: 1,
-        isCorrect: false,
-        isCurrent: false,
-      },
-      {
-        url:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
-        id: 2,
-        isCorrect: false,
-        isCurrent: false,
-      },
-      {
-        url:
-          "https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        id: 3,
-        isCorrect: false,
-        isCurrent: false,
-      },
-      {
-        url:
-          "https://images.pexels.com/photos/2662434/pexels-photo-2662434.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        id: 4,
-        isCorrect: false,
-        isCurrent: false,
-      },
-    ],
+    words: shuffleArray(words),
+    imgs: shuffleArray(imgs),
   });
 
   // Match Section
+
+  const englishWords = [
+    { id: 1, word: "Hello", isCorrect: false, isCurrent: false },
+    { id: 2, word: "Bye", isCorrect: false, isCurrent: false },
+    { id: 3, word: "How are you?", isCorrect: false, isCurrent: false },
+    { id: 4, word: "Where am I?", isCorrect: false, isCurrent: false },
+  ];
+  const frenchWords = [
+    { id: 1, word: "Salut", isCorrect: false, isCurrent: false },
+    { id: 2, word: "Au revoir", isCorrect: false, isCurrent: false },
+    { id: 3, word: "Comment ca va", isCorrect: false, isCurrent: false },
+    { id: 4, word: "Ou je suis", isCorrect: false, isCurrent: false },
+  ];
+
   const [cards, updateCards] = useState({
-    englishWords: [
-      { id: 1, word: "Hello", isCorrect: false, isCurrent: false },
-      { id: 2, word: "Bye", isCorrect: false, isCurrent: false },
-      { id: 3, word: "How are you?", isCorrect: false, isCurrent: false },
-      { id: 4, word: "Where am I?", isCorrect: false, isCurrent: false },
-    ],
-    frenchWords: [
-      { id: 1, word: "Salut", isCorrect: false, isCurrent: false },
-      { id: 2, word: "Au revoir", isCorrect: false, isCurrent: false },
-      { id: 3, word: "Comment ca va", isCorrect: false, isCurrent: false },
-      { id: 4, word: "Ou je suis", isCorrect: false, isCurrent: false },
-    ],
+    englishWords: shuffleArray(englishWords),
+    frenchWords: shuffleArray(frenchWords),
   });
 
   // Conjugation Section
-  const [conjugationWords, updateConjugationWords] = useState([
+
+  const conjugationContent = [
     { id: 1, inputWord: "suis", setup: "Je", isCorrect: false },
     { id: 2, inputWord: "es", setup: "Tu", isCorrect: false },
     { id: 3, inputWord: "sommes", setup: "Nous", isCorrect: false },
     { id: 4, inputWord: "sont", setup: "Ils", isCorrect: false },
-  ]);
+  ];
+  const [conjugationWords, updateConjugationWords] = useState(
+    shuffleArray(conjugationContent)
+  );
 
   // Fill In The Blank Section
   const [fITBWord, updateFITBWord] = useState({
@@ -86,6 +96,21 @@ const BasicsContainer = (props) => {
 
   const [section, updateSection] = useState(0);
   let lessonProgress = (section * 100) / 4;
+
+  function shuffleArray(array) {
+    let curId = array.length;
+    // There remain elements to shuffle
+    while (0 !== curId) {
+      // Pick a remaining element
+      let randId = Math.floor(Math.random() * curId);
+      curId -= 1;
+      // Swap it with the current element.
+      let tmp = array[curId];
+      array[curId] = array[randId];
+      array[randId] = tmp;
+    }
+    return array;
+  }
 
   let currentSection = () => {
     if (section === 0) {
