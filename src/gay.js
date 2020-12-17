e7563a0a7f8854b8d6f6a34e2bd71f28ff68f66b
martimafonso <49534332+martimafonso@users.noreply.github.com>
2020-12-17 08:58:30 +0000

    added gay in main page (cuz of testing)

---
+++ src/gay.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MainHeader from "../headers/MainHeader";
+
+const Gay = () => {
+  return (
+    <div>
+      <MainHeader />
+      <h2> Gay </h2>
+    </div>
+  );
+};
+
+export default Gay;
